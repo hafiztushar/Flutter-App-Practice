dcacc8ee780c74c9880650854dd34d333f16e754
hafiztushar <hafiztushar@gmail.com>
2020-07-15 21:39:47 +0600

    Stateless Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('MY FIRST APP'),
-          centerTitle: true,
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MY FIRST APP'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text(
+          'hello ninjas',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
+}
